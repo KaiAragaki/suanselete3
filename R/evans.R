abee70be50410f8355ce0152cdc0ca7fb7e9cdca
Kai Aragaki <kaiaragaki@proton.me>
2025-11-15 15:08:28 +0100

    feat: add Evans County sample dataset
    This dataset is used in the practice exercises of chapter 2. It's
    included purely for convenience, to avoid having to do manual data entry -
    all the data exist in the book.

---
+++ R/evans.R
@@ -0,0 +1,15 @@
+#' Evans County dataset
+#'
+#' A sample of 50 patients from the Evans County Heart Study dataset.
+#'
+#' This dataset is used in the practice questions for Chapter 2.
+#'
+#' @format A data.frame with 50 rows and the following variables:
+#' \describe{
+#' \item{chr}{1 = history of chronic disease, 0 = no history of chronic disease}
+#' \item{time}{Survival time (years)}
+#' \item{event}{1 = patient diagnosed with coronary heart disease, 0 = censored}
+#' }
+#' @source Kleinbaum, D.G. and Klein, M. (2012) Survival Analysis - A
+#'     Self-Learning Text, 3rd ed., Springer
+"evans"
